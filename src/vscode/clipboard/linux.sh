f559c71970260c9bb9b72fc392063dc8454a272e
Your Name <your_email@example.com>
2026-03-25 11:36:21 +0800

    fix(linux): use wl-paste for wayland clipboard
    ci: add workflow to build and upload VSIX artifacts

--- src/vscode/clipboard/linux.sh
+++ src/vscode/clipboard/linux.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
-if [ -z "$DISPLAY" ]; then
+if [ -z "$DISPLAY" ] && [ -z "$WAYLAND_DISPLAY" ]; then
     echo "no support" >&2
     exit 1
 fi
 
 case "$XDG_SESSION_TYPE" in
 wayland)
-    command -v wl-copy >/dev/null 2>&1 || {
+    command -v wl-paste >/dev/null 2>&1 || {
         echo >&2 "no wl-clipboard"
         exit 1
     }
-    filePath=$(wl-copy -o 2>/dev/null | grep ^file:// | cut -c8-)
+    filePath=$(wl-paste -t text/uri-list 2>/dev/null | grep ^file:// | cut -c8-)
     if [ -z "$filePath" ]; then
         if
-            wl-copy -t image/png image/png -o >"$1" 2>/dev/null
+            wl-paste -t image/png >"$1" 2>/dev/null
         then
             echo "$1"
         else
